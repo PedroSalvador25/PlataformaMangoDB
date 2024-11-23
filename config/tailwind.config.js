1ba45f7cbf1345ca1c4aa0ce4bf97898c15d7456
jose8a <Jose8aDev@gmail.com>
2024-11-23 12:26:24 -0700

    tailwind-fix

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,9 +1,10 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
+    './public/*.html',
     './app/helpers/**/*.rb',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './app/views/**/*',
   ],
   theme: {
     extend: {},
