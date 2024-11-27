81745cf39c8861e9bca7b3b494c007d706685d1e
moniicangulo <angulosmonica@gmail.com>
2024-11-27 17:37:04 -0600

    migracion de plantas y hectareas

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,14 @@
 class Plant < ApplicationRecord
+    belongs_to :hectare
+
+    validate :max_plants_in_hectare
+
+  private
+
+  def max_plants_in_hectare
+    if Plant.where(hectare_id: self.hectare_id).count >= 25
+      errors.add(:base, "Una hectárea no puede tener más de 25 plantas")
+    end
+  end
+
 end
--- db/migrate/20241127025200_drop_werehouse_table.rb
+++
@@ -1,6 +0,0 @@
-class DropWerehouseTable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :werehouses
-  end
-end
-
